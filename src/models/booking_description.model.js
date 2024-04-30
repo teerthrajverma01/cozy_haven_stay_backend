d3ee2cdc73231a8899ea3873b766d257f23d669b
teerthrajverma01 <teerthrajverma181001@gmail.com>
2024-04-30 10:25:14 +0530

    modified  booking detai;+description model

--- src/models/booking_description.model.js
+++ src/models/booking_description.model.js
@@ -20,7 +20,7 @@ const BookingDescription = db.define(
     },
     room_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+
       references: {
         model: "room_detail",
         key: "room_id",
@@ -57,3 +57,15 @@ const BookingDescription = db.define(
 // });
 
 module.exports = BookingDescription;
+
+// DROP TABLE IF EXISTS booking_description;
+// CREATE TABLE IF NOT EXISTS booking_description (
+// 	uid INT PRIMARY KEY AUTO_INCREMENT,
+//     booking_id INT NOT NULL,
+//     room_id INT,
+// 	booking_amount_room INT,
+//     checkin_date Date,
+//     checkout_date Date,
+//     CONSTRAINT  FK_bd_bd FOREIGN KEY (booking_id) REFERENCES booking_detail(booking_id),
+//     CONSTRAINT  FK_bd_rd FOREIGN KEY (room_id) REFERENCES room_detail(room_id)
+// 	);
--- src/models/booking_detail.model.js
+++ src/models/booking_detail.model.js
@@ -12,7 +12,7 @@ const BookingDetail = db.define(
     },
     hotel_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+
       references: {
         model: "hotel_detail",
         key: "hotel_id",
@@ -20,7 +20,6 @@ const BookingDetail = db.define(
     },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: "user_detail",
         key: "user_id",
@@ -70,3 +69,17 @@ const BookingDetail = db.define(
 // });
 
 module.exports = BookingDetail;
+
+// DROP TABLE IF EXISTS booking_detail;
+// CREATE TABLE IF NOT EXISTS booking_detail (
+// 	booking_id INT PRIMARY KEY AUTO_INCREMENT,
+//     hotel_id INT,
+//     user_id INT,
+//     no_rooms INT ,
+//     total_booking_amount INT,
+//     checkin_date Date,
+//     checkout_date Date,
+//     booking_status ENUM('BOOKED', 'REFUND_PENDING','REFUND_APPROVED','REFUND_CANCELED'),
+//     CONSTRAINT  FK_bd_hd FOREIGN KEY (hotel_id) REFERENCES hotel_detail(hotel_id),
+//     CONSTRAINT  FK_bd_ud FOREIGN KEY (user_id) REFERENCES user_detail(user_id)
+// 	);
