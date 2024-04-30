0e28510f964018e5b56f3727e0108ce65cf14232
teerthrajverma01 <teerthrajverma181001@gmail.com>
2024-04-30 10:34:11 +0530

    modified review model

--- src/models/review_detail.model.js
+++ src/models/review_detail.model.js
@@ -43,3 +43,13 @@ const ReviewDetail = db.define(
 // });
 
 module.exports = ReviewDetail;
+
+// DROP TABLE IF EXISTS review_detail;
+// CREATE TABLE IF NOT EXISTS review_detail (
+// 	review_id INT PRIMARY KEY AUTO_INCREMENT,
+//     booking_id INT NOT NULL,
+//     review TEXT,
+//     rating FLOAT(2,1),
+//     time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+//     CONSTRAINT FK_rd_bd FOREIGN KEY (booking_id) REFERENCES booking_detail(booking_id)
+// 	);
